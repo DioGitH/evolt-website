f75fbe9d986f09c3b59ea801228cf1bb517a0b72
Maulidio <maulidiobisnis16@gmail.com>
2024-03-20 08:38:45 +0700

    add Dashboard Page

---
+++ src/evolt-website/src/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/navbar";
+
+export default function Dashboard(){
+    return(
+        <div>
+            <div className="container">
+                <div className="navbar">
+                    <Navbar/>
+                </div>
+                <div className="content">
+
+                </div>
+            </div>
+        </div>
+    )
+}
