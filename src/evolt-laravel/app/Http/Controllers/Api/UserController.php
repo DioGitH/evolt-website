edb1f87363beb839aa6fba921ec5af29b21b9a0a
rizkiraden <rizkiraden616@gmail.com>
2024-05-21 15:02:46 +0700

    Commit User Post API

--- src/evolt-laravel/app/Http/Controllers/Api/UserController.php
+++ src/evolt-laravel/app/Http/Controllers/Api/UserController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -14,5 +15,36 @@ class UserController extends Controller
         $user = User::all();
 
         return new PostResource(true, 'List Data User', $user);
+    }
+
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'email' => 'required',
+            'pin' => 'required',
+            'photo_profile' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //upload photo_profile
+        $photo_profile = $request->file('photo_profile');
+        $photo_profile->storeAs('public/users', $photo_profile->hashName());
+
+        //create post
+        $user = User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'pin' => $request->pin,
+            'photo_profile' => $photo_profile->hashName(),
+        ]);
+
+        //return response
+        return new PostResource(true, 'Data User Berhasil Ditambahkan!', $user);
     }
 }
